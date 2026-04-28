06b62694d7f6819aaed875e9f448eaf6f37ef462
jorgeEPAM <jorge_vizuete@epamneoris.com>
2026-04-28 20:41:02 +0200

    Fixing cors

--- backend/src/main/java/com/example/backend_v2/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend_v2/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://coflow-frontend.local"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
